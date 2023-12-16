8c5528edb11af6cf0f76397c6945c48c9fa92719
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2023-12-16 06:03:02 +0000

    Feature: Remove checking OAuth2Required logic

--- oauth/get_token_context.go
+++ oauth/get_token_context.go
@@ -8,11 +8,8 @@ import (
 )
 
 func GetTokenCtx(
-	oauth2Required bool, nfType models.NfType, nfId, nrfUri, scope, targetNF string,
+	nfType models.NfType, nfId, nrfUri, scope, targetNF string,
 ) (context.Context, *models.ProblemDetails, error) {
-	if !oauth2Required {
-		return context.TODO(), nil, nil
-	}
 	tok, pd, err := SendAccTokenReq(nfId, nfType, scope, targetNF, nrfUri)
 	if err != nil {
 		return nil, pd, err
