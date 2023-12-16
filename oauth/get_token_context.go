e15a4ff046f6ccabd2d498d789f1c6c0059ba6b6
Tim Liu <timliu@nctu.edu.tw>
2023-12-16 09:43:13 +0000

    merge send_accesstoken.go to get_token_context.go

--- oauth/get_token_context.go
+++ oauth/get_token_context.go
@@ -2,18 +2,85 @@ package oauth
 
 import (
 	"context"
+	"sync"
+	"time"
+
+	"github.com/antihax/optional"
+	"golang.org/x/oauth2"
 
 	"github.com/free5gc/openapi"
+	"github.com/free5gc/openapi/Nnrf_AccessToken"
 	"github.com/free5gc/openapi/models"
 )
 
+var tokenMap sync.Map
+var clientMap sync.Map
+
 func GetTokenCtx(
-	nfType models.NfType, nfId, nrfUri, scope, targetNF string,
+	nfType models.NfType,
+	nfId, nrfUri, scope, targetNF string,
 ) (context.Context, *models.ProblemDetails, error) {
-	tok, pd, err := SendAccTokenReq(nfId, nfType, scope, targetNF, nrfUri)
+	tok, pd, err := sendAccTokenReq(nfType, nfId, nrfUri, scope, targetNF)
 	if err != nil {
 		return nil, pd, err
 	}
 	return context.WithValue(context.Background(),
 		openapi.ContextOAuth2, tok), pd, nil
+}
+
+func sendAccTokenReq(
+	nfType models.NfType,
+	nfId, nrfUri, scope, targetNF string,
+) (oauth2.TokenSource, *models.ProblemDetails, error) {
+	var client *Nnrf_AccessToken.APIClient
+
+	configuration := Nnrf_AccessToken.NewConfiguration()
+	configuration.SetBasePath(nrfUri)
+
+	if val, ok := clientMap.Load(configuration); ok {
+		client = val.(*Nnrf_AccessToken.APIClient)
+	} else {
+		client = Nnrf_AccessToken.NewAPIClient(configuration)
+		clientMap.Store(configuration, client)
+	}
+
+	var tok models.AccessTokenRsp
+	if val, ok := tokenMap.Load(scope); ok {
+		tok = val.(models.AccessTokenRsp)
+		if int32(time.Now().Unix()) < tok.ExpiresIn {
+			token := &oauth2.Token{
+				AccessToken: tok.AccessToken,
+				TokenType:   tok.TokenType,
+				Expiry:      time.Unix(int64(tok.ExpiresIn), 0),
+			}
+			return oauth2.StaticTokenSource(token), nil, nil
+		}
+	}
+	tok, res, err := client.AccessTokenRequestApi.AccessTokenRequest(
+		context.Background(), "client_credentials",
+		nfId, scope, &Nnrf_AccessToken.AccessTokenRequestParamOpts{
+			NfType:       optional.NewInterface(nfType),
+			TargetNfType: optional.NewInterface(targetNF),
+		})
+
+	if err == nil {
+		tokenMap.Store(scope, tok)
+		token := &oauth2.Token{
+			AccessToken: tok.AccessToken,
+			TokenType:   tok.TokenType,
+			Expiry:      time.Unix(int64(tok.ExpiresIn), 0),
+		}
+		return oauth2.StaticTokenSource(token), nil, nil
+	} else if res != nil {
+		if res.Status != err.Error() {
+			return nil, nil, err
+		}
+		accesstoken_err := err.(openapi.GenericOpenAPIError).Model().(models.AccessTokenErr)
+		pd := &models.ProblemDetails{
+			AccessTokenError: &accesstoken_err,
+		}
+		return nil, pd, err
+	} else {
+		return nil, nil, openapi.ReportError("server no response")
+	}
 }
