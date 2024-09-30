2f41246a60e56cfd1bf146e7ec41347017d60006
chh <chh.cs12@nycu.edu.tw>
2024-09-30 07:39:47 +0000

    style: correct spell mistake

--- models/model_pcf_am_policy_control_policy_association_request.go
+++ models/model_pcf_am_policy_control_policy_association_request.go
@@ -53,7 +53,7 @@ type PcfAmPolicyControlPolicyAssociationRequest struct {
 	// array of allowed S-NSSAIs for the Non-3GPP access.
 	N3gAllowedSnssais []Snssai    `json:"n3gAllowedSnssais,omitempty" yaml:"n3gAllowedSnssais" bson:"n3gAllowedSnssais,omitempty"`
 	Guami             *Guami      `json:"guami,omitempty" yaml:"guami" bson:"guami,omitempty"`
-	ServiveName       ServiceName `json:"serviveName,omitempty" yaml:"serviveName" bson:"serviveName,omitempty"`
+	ServiceName       ServiceName `json:"serviceName,omitempty" yaml:"serviceName" bson:"serviceName,omitempty"`
 	TraceReq          *TraceData  `json:"traceReq,omitempty" yaml:"traceReq" bson:"traceReq,omitempty"`
 	NwdafDatas        []NwdafData `json:"nwdafDatas,omitempty" yaml:"nwdafDatas" bson:"nwdafDatas,omitempty"`
 	// A string used to indicate the features supported by an API that is used as defined in clause  6.6 in 3GPP TS 29.500. The string shall contain a bitmask indicating supported features in  hexadecimal representation Each character in the string shall take a value of \"0\" to \"9\",  \"a\" to \"f\" or \"A\" to \"F\" and shall represent the support of 4 features as described in  table 5.2.2-3. The most significant character representing the highest-numbered features shall  appear first in the string, and the character representing features 1 to 4 shall appear last  in the string. The list of features and their numbering (starting with 1) are defined  separately for each API. If the string contains a lower number of characters than there are  defined features for an API, all features that would be represented by characters that are not  present in the string are not supported.
