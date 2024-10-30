293df1d1cd8a27cf0c6d18e97b440fc1d20c9736
pf-lin <welin9211@gmail.com>
2024-10-30 06:56:23 +0000

    fix: add multipart tags in ModifyPduSessionRequest

--- models/model_modify_pdu_session_request.go
+++ models/model_modify_pdu_session_request.go
@@ -13,10 +13,10 @@
 package models
 
 type ModifyPduSessionRequest struct {
-	JsonData                    *VsmfUpdateData `json:"jsonData,omitempty" yaml:"jsonData" bson:"jsonData,omitempty"`
-	BinaryDataN1SmInfoToUe      []byte          `json:"binaryDataN1SmInfoToUe,omitempty" yaml:"binaryDataN1SmInfoToUe" bson:"binaryDataN1SmInfoToUe,omitempty"`
-	BinaryDataN4Information     []byte          `json:"binaryDataN4Information,omitempty" yaml:"binaryDataN4Information" bson:"binaryDataN4Information,omitempty"`
-	BinaryDataN4InformationExt1 []byte          `json:"binaryDataN4InformationExt1,omitempty" yaml:"binaryDataN4InformationExt1" bson:"binaryDataN4InformationExt1,omitempty"`
-	BinaryDataN4InformationExt2 []byte          `json:"binaryDataN4InformationExt2,omitempty" yaml:"binaryDataN4InformationExt2" bson:"binaryDataN4InformationExt2,omitempty"`
-	BinaryDataN4InformationExt3 []byte          `json:"binaryDataN4InformationExt3,omitempty" yaml:"binaryDataN4InformationExt3" bson:"binaryDataN4InformationExt3,omitempty"`
+	JsonData                    *VsmfUpdateData `json:"jsonData,omitempty" yaml:"jsonData" bson:"jsonData,omitempty" multipart:"contentType:application/json,omitempty"`
+	BinaryDataN1SmInfoToUe      []byte          `json:"binaryDataN1SmInfoToUe,omitempty" yaml:"binaryDataN1SmInfoToUe" bson:"binaryDataN1SmInfoToUe,omitempty" multipart:"contentType:application/vnd.3gpp.5gnas,ref:JsonData.N1SmInfoToUe.ContentId,omitempty"`
+	BinaryDataN4Information     []byte          `json:"binaryDataN4Information,omitempty" yaml:"binaryDataN4Information" bson:"binaryDataN4Information,omitempty" multipart:"contentType:application/vnd.3gpp.pfcp,ref:JsonData.N4Info.N4MessagePayload.ContentId,omitempty"`
+	BinaryDataN4InformationExt1 []byte          `json:"binaryDataN4InformationExt1,omitempty" yaml:"binaryDataN4InformationExt1" bson:"binaryDataN4InformationExt1,omitempty" multipart:"contentType:application/vnd.3gpp.pfcp,ref:JsonData.N4InfoExt1.N4MessagePayload.ContentId,omitempty"`
+	BinaryDataN4InformationExt2 []byte          `json:"binaryDataN4InformationExt2,omitempty" yaml:"binaryDataN4InformationExt2" bson:"binaryDataN4InformationExt2,omitempty" multipart:"contentType:application/vnd.3gpp.pfcp,ref:JsonData.N4InfoExt2.N4MessagePayload.ContentId,omitempty"`
+	BinaryDataN4InformationExt3 []byte          `json:"binaryDataN4InformationExt3,omitempty" yaml:"binaryDataN4InformationExt3" bson:"binaryDataN4InformationExt3,omitempty" multipart:"contentType:application/vnd.3gpp.pfcp,ref:JsonData.N4InfoExt3.N4MessagePayload.ContentId,omitempty"`
 }
